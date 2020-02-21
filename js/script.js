e82eca054eac9672bba23a271c97b639b314d2d6
lorrainekamanda <lorrainekamanda@gmail.com>
2020-02-21 21:55:46 +0300

    adding design toggle function

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#icon1").click(function(){
+    $("#content").toggle()
+    $("#icon1").toggle( )
+  
+ })
+ $("#content").click(function(){
+  $("#icon1").toggle()
+  $("#content").toggle()
+})
+
+
+  
+})
+
